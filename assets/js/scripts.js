939d9bd5765a52dddffe50112c52dd585a34d154
Peter Olesen <63056450+peter-olesen@users.noreply.github.com>
2024-02-13 14:50:13 +0100

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,16 +1,27 @@
-fetchCategoryList();
+// fetchCategoryList();
 fetchProductData();
 
 // Functions below
 
-function fetchCategoryList(){
+function fetchCategoryList() {
     fetch('https://dummyjson.com/products/categories')
         .then(res => res.json())
         .then(console.log);
 };
 
-function fetchProductData(){
+function fetchProductData() {
     fetch('https://dummyjson.com/products?limit=0')
         .then(res => res.json())
-        .then(console.log);
+        .then(productData => {
+            receivedProductData(productData);
+        })
 };
+
+function receivedProductData(productData) {
+    let allProducts = productData.products
+    let featuredProducts = [];
+
+    featuredProducts.push(allProducts[3], allProducts[5], allProducts[9])
+
+    console.log(featuredProducts);
+}
